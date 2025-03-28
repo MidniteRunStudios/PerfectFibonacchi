e53227c40d2c03be642d5c896c1c9bf71d56ce58
Midnite Run Studios <cybernetikshadow@hotmail.com>
2025-03-27 21:18:27 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = f(2000000000);
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+        public static BigInteger f(int i)
+        {
+            if (i == 0)
+            {
+                return 0;
+            }
+            if (i == 1)
+            {
+                return 1;
+            }
+            //return f(i - 1) + f(i - 2);
+            BigInteger a = 0;
+            BigInteger b = 1;
+            BigInteger c = 0;
+            for (int n = 2; n <= i; ++n)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+
+        }
+    }
+}
